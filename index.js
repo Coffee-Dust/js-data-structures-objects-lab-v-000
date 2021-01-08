2b8da9f1c461c3aa3e31217c0e6e47e5f01e623d
John Dust <d24dust@hotmail.com>
2021-01-08 17:33:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
-const driver = {name: "Maria"}
+const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
   const copy = Object.assign({}, driver)
