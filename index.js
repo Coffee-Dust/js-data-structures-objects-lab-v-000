9af249e8e2fa51ad83e8f7ea75e19ae8b0ab47aa
John Dust <d24dust@hotmail.com>
2021-01-08 17:22:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const driver = {name: "Maria"}
 function updateDriverWithKeyAndValue(driver, key, value) {
   const copy = Object.assign({}, driver)
   copy[key] = value
-  console.log(`driver copy is: ${copy}, ${key}, ${value}`)
   return copy
 }
